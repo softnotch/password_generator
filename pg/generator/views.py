6bbf2a5ac930964591608687f80a4d7c1b6d4248
kelvin <uchekelvin3@gmail.com>
2020-06-06 22:34:01 +0100

    feat:random password generated

--- pg/generator/views.py
+++ pg/generator/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 # # Create your views here.
 # def home(request):
@@ -13,7 +14,18 @@ def home(request):
 
 
 def password(request):
-    return render(request, 'generator/password.html')
+
+
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+
+    length = 10
+
+    thepassword = ''
+
+    for x in range(length):
+        thepassword+= random.choice(characters)
+
+    return render(request, 'generator/password.html', {'password':thepassword})
 
 
 def about(request):
